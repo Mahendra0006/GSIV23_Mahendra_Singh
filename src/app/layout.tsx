ea752706ead331e82b6ec5e0d3284c4109174b3a
Mahendra Singh <mahendrasinghc06@gmail.com>
2023-08-19 15:38:43 +0530

    fix: main fixed type error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 'use client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Provider } from 'react-redux'
 import store from '@/Redux/store' // Import the Redux Toolkit store
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Movie App',
-  description: 'IMDB movie list rating ',
-}
 
 export default function RootLayout({
   children,
